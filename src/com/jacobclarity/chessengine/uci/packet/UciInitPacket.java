5c11da83300fdfc41c0739694cbafb0f118f6e0e
Jacob Clarity <jclarity0@gmail.com>
2017-11-26 13:09:37 -0600

    Added UciInitPacket

---
+++ src/com/jacobclarity/chessengine/uci/packet/UciInitPacket.java
@@ -0,0 +1,11 @@
+package com.jacobclarity.chessengine.uci.packet;
+
+//Sent by engine once id and options are sent to tell GUI it is ready for commands
+public class UciInitPacket implements UciPacket
+{
+    @Override
+    public String toCommandString()
+    {
+        return "uciok";
+    }
+}
