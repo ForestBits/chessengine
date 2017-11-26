0c2b84935816b7b5527634bb72311f99a7acc693
Jacob Clarity <jclarity0@gmail.com>
2017-11-26 13:09:23 -0600

    Added InfoPacket

---
+++ src/com/jacobclarity/chessengine/uci/packet/InfoPacket.java
@@ -0,0 +1,13 @@
+package com.jacobclarity.chessengine.uci.packet;
+
+//sent by engine to tell GUI about search progress and statistics
+public class InfoPacket implements UciPacket
+{
+    //TODO: implement
+
+    @Override
+    public String toCommandString()
+    {
+        return null;
+    }
+}
