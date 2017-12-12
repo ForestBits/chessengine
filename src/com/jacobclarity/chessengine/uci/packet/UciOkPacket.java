fbe121347e414f7d12e1de9fb24ad53e43244159
Jacob Clarity <jacobaclarity@gmail.com>
2017-12-11 19:05:18 -0600

    Changed name of packet to reflect name in UCI protocol for it

--- src/com/jacobclarity/chessengine/uci/packet/UciOkPacket.java
+++ src/com/jacobclarity/chessengine/uci/packet/UciOkPacket.java
@@ -1,7 +1,7 @@
 package com.jacobclarity.chessengine.uci.packet;
 
 //Sent by engine once id and options are sent to tell GUI it is ready for commands
-public class UciInitPacket implements UciPacket
+public class UciOkPacket implements UciPacket
 {
     @Override
     public String toCommandString()
