b9ca7cffe06d9fde11c903aa806a9eb659afc459
Jacob Clarity <jacobaclarity@gmail.com>
2017-12-11 19:04:44 -0600

    Renamed packet type to reflect UCI protocol name for it

--- src/com/jacobclarity/chessengine/uci/packet/ReadyOkPacket.java
+++ src/com/jacobclarity/chessengine/uci/packet/ReadyOkPacket.java
@@ -1,7 +1,7 @@
 package com.jacobclarity.chessengine.uci.packet;
 
 //sent by engine in response to IsReadyPacket - indicates engine is ready to receive more commands
-public class EngineReadyPacket implements UciPacket
+public class ReadyOkPacket implements UciPacket
 {
     @Override
     public String toCommandString()
